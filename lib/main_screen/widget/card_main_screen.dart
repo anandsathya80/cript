cdf7d3c84c80c29edad60b7811c42427307c4ac8
anandsathya80 <anandsathya80@gmail.com>
2024-12-30 16:19:11 +0700

    fix: main screen: add refresh indicator

--- lib/main_screen/widget/card_main_screen.dart
+++ lib/main_screen/widget/card_main_screen.dart
@@ -25,157 +25,179 @@ class _CardMainScreenState extends State<CardMainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<CryptoData>?>(
-      future: cryptoDataFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return const Center(child: Text('No data available'));
-        }
+    return RefreshIndicator(
+      onRefresh: () async {
+        setState(() {
+          cryptoDataFuture = fetchCryptoData();
+        });
+        await cryptoDataFuture;
+      },
+      child: FutureBuilder<List<CryptoData>?>(
+        future: cryptoDataFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No data available'));
+          }
 
-        final items = snapshot.data!;
-        return ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Stack(
-                  children: <Widget>[
-                    InkWell(
-                      onTap: () async {
-                        final data = await fetchCryptoDetails(items[index].id);
-                        if (data != null) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => DetailScreen(data: data),
+          final items = snapshot.data!;
+          return ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Stack(
+                    children: <Widget>[
+                      InkWell(
+                        onTap: () async {
+                          final data =
+                              await fetchCryptoDetails(items[index].id);
+                          if (data != null) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailScreen(data: data),
+                              ),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'Failed to fetch details for ${items[index].name}'),
+                              ),
+                            );
+                          }
+                        },
+                        child: Container(
+                          height: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(_borderRadius),
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.blueAccent,
+                                Colors.blueGrey,
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
                             ),
-                          );
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(
-                                  'Failed to fetch details for ${items[index].name}'),
-                            ),
-                          );
-                        }
-                      },
-                      child: Container(
-                        height: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(_borderRadius),
-                          gradient: LinearGradient(
-                            colors: [
-                              items[index].startColor,
-                              items[index].endColor,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.blueGrey.withOpacity(0.6),
+                                blurRadius: 12,
+                                offset: const Offset(0, 6),
+                              ),
                             ],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
                           ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: items[index].endColor,
-                              blurRadius: 12,
-                              offset: const Offset(0, 6),
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 2,
+                              child: Image.network(
+                                items[index].imageUrl,
+                                height: 64,
+                                width: 64,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Icon(
+                                    Icons.error,
+                                    color: Colors.red,
+                                  );
+                                },
+                              ),
+                            ),
+                            Expanded(
+                              flex: 4,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    items[index].name,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Avenir',
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  Text(
+                                    items[index].symbol.toUpperCase(),
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Avenir',
+                                    ),
+                                  ),
+                                  const SizedBox(height: 16),
+                                  Text(
+                                    "\$${items[index].priceUsd.toStringAsFixed(2)}",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Avenir',
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(
+                                    "${items[index].percentChange24h.toStringAsFixed(2)}% (24h)",
+                                    style: TextStyle(
+                                      color: items[index].percentChange24h < 0
+                                          ? Colors.red
+                                          : Colors.green,
+                                      fontFamily: 'Avenir',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         ),
                       ),
-                    ),
-                    Positioned.fill(
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            flex: 2,
-                            child: Container(),
-                            // Icon(CryptoFontIcons().items[index].symbol),
-                          ),
-                          Expanded(
-                            flex: 4,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  items[index].name,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Avenir',
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                                Text(
-                                  items[index].symbol,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Avenir',
-                                  ),
-                                ),
-                                const SizedBox(height: 16),
-                                Text(
-                                  "\$${items[index].priceUsd}",
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Avenir',
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                Text(
-                                  "${items[index].percentChange24h}% (24h)",
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Avenir',
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
-        );
-      },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
 
 class CryptoData {
   final String id;
-  final String name;
+  final int market_cap_rank;
   final String symbol;
-  final String priceUsd;
-  final String percentChange24h;
-  final Color startColor;
-  final Color endColor;
+  final String name;
+  final String imageUrl;
+  final double priceUsd;
+  final double percentChange24h;
 
   CryptoData({
     required this.id,
-    required this.name,
+    required this.market_cap_rank,
     required this.symbol,
+    required this.name,
+    required this.imageUrl,
     required this.priceUsd,
     required this.percentChange24h,
-    required this.startColor,
-    required this.endColor,
   });
 
   factory CryptoData.fromJson(Map<String, dynamic> json) {
     return CryptoData(
       id: json['id'],
-      name: json['name'],
       symbol: json['symbol'],
-      priceUsd: json['price_usd'],
-      percentChange24h: json['percent_change_24h'],
-      startColor: Colors.blue,
-      endColor: Colors.green,
+      name: json['name'],
+      imageUrl: json['image'],
+      priceUsd: json['current_price'].toDouble(),
+      percentChange24h: json['price_change_percentage_24h']?.toDouble() ?? 0.0,
+      market_cap_rank: json['market_cap_rank'],
     );
   }
 }
@@ -213,30 +235,28 @@ class CustomCardShapePainter extends CustomPainter {
   }
 }
 
-Future<List<CryptoData>?> fetchCryptoData() async {
-  final response =
-      await http.get(Uri.parse('https://api.coinlore.net/api/tickers/'));
+Future<List<CryptoData>> fetchCryptoData() async {
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
+
+  final response = await http.get(Uri.parse(url));
+
   if (response.statusCode == 200) {
-    final List<dynamic> data = json.decode(response.body)['data'];
-    return data.map((item) => CryptoData.fromJson(item)).toList();
+    final List<dynamic> data = json.decode(response.body);
+    return data.map((json) => CryptoData.fromJson(json)).toList();
   } else {
-    throw Exception('Failed to load data');
+    throw Exception('Failed to fetch cryptocurrency data');
   }
 }
 
 Future<Map<String, dynamic>?> fetchCryptoDetails(String id) async {
-  final url = 'https://api.coinlore.net/api/ticker/?id=$id';
-  try {
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      return data.isNotEmpty ? data[0] : null; // Ambil item pertama
-    } else {
-      print('Failed to load details: ${response.statusCode}');
-      return null;
-    }
-  } catch (e) {
-    print('Error fetching details: $e');
+  final url = 'https://api.coingecko.com/api/v3/coins/$id';
+
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body) as Map<String, dynamic>;
+  } else {
+    print("Error fetching details: ${response.statusCode}");
     return null;
   }
 }
